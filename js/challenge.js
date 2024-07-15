347c76b67dcc7d22cf96fba4abe10f4232d2efaf
brendan544 <brendan.gwer@student.moringaschool.com>
2024-07-15 17:40:16 +0300

    complete lab

---
+++ js/challenge.js
@@ -0,0 +1,10 @@
+const scriptElement = document.createElement('script');
+
+// Set attributes for the script element
+scriptElement.src = 'js/index.min.js'; // Replace with your script file URL
+scriptElement.type = 'text/javascript';
+
+// Optional: Add event listeners or any other attributes as needed
+
+// Append the script element to the document's head
+document.head.appendChild(scriptElement);
